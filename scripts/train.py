59c70b0bb0ed5df5a232a4908e74da9a548ffb01
Lucas Faria <lucas.meneghesso.faria@gmail.com>
2020-09-10 22:18:43 -0300

    Added the use of sklearn Pipeline

--- scripts/train.py
+++ scripts/train.py
@@ -4,6 +4,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 import nltk
 from joblib import dump
+from sklearn.pipeline import Pipeline
 
 pos_train_raw_folder = (Path('..') / 'data' / 'aclImdb_v1/aclImdb/train/pos').resolve()
 neg_train_raw_folder = (Path('..') / 'data' / 'aclImdb_v1/aclImdb/train/neg').resolve()
@@ -31,22 +32,25 @@ for file in neg_test_raw_folder.iterdir():
     with open(file, 'r', encoding='utf-8') as f:
         neg_test_raw.append(f.read())
 
-countvec = CountVectorizer(stop_words=nltk.corpus.stopwords.words('english'), binary=False,
-                          min_df=0.003, max_df=0.98, ngram_range=(1, 2)).fit(pos_train_raw + pos_test_raw + neg_train_raw + neg_test_raw)
+word_occur = False
+use_idf = True
+min_df=0.003
+max_df=0.98
+ngram_range = (1, 2)
 
-#Prepare train data:
-X_train = countvec.transform(pos_train_raw + pos_test_raw + neg_train_raw + neg_test_raw)
-tfidf = TfidfTransformer(use_idf=True).fit(X_train)
-X_train = tfidf.transform(X_train)
+pipe = Pipeline([
+    ('countvec', CountVectorizer(stop_words=nltk.corpus.stopwords.words('english'), binary=word_occur, min_df=min_df, max_df=max_df, ngram_range=ngram_range)),
+    ('tfidf', TfidfTransformer(use_idf=use_idf)),
+    ('naivebayes', MultinomialNB())
+])
 
-n_pos_samples_train = len(pos_train_raw + pos_test_raw)
-n_neg_samples_train = len(neg_train_raw + neg_test_raw)
+#Prepare train labels:
+n_pos_samples_train = len(pos_train_raw)
+n_neg_samples_train = len(neg_train_raw)
 y_train = np.concatenate((np.ones((n_pos_samples_train)), np.zeros((n_neg_samples_train))))
 
-#Fit model:
-naivebayes = MultinomialNB()
-naivebayes.fit(X_train, y_train)
+pipe.fit(pos_train_raw + neg_train_raw, y_train)
 
-file_name = 'naivebayes.joblib'
+file_name = 'pipeline_naivebayes.joblib'
 path = Path('../models')
-dump(naivebayes, path / file_name)
+dump(pipe, path / file_name)
