5de1ea101cc21ffce4619ed7527a5e3c6e903ec8
Lucas Faria <lucas.meneghesso.faria@gmail.com>
2020-09-12 16:25:25 -0300

    Added the use of GridSearchCV for hyperparameter tunning

--- scripts/train.py
+++ scripts/train.py
@@ -32,10 +32,11 @@ for file in neg_test_raw_folder.iterdir():
     with open(file, 'r', encoding='utf-8') as f:
         neg_test_raw.append(f.read())
 
-word_occur = False
-use_idf = True
-min_df=0.003
-max_df=0.98
+#Best params from GridSearchCV (notebook):
+word_occur = True
+use_idf = False
+min_df=0.001
+max_df=0.9
 ngram_range = (1, 2)
 
 pipe = Pipeline([
@@ -44,12 +45,12 @@ pipe = Pipeline([
     ('naivebayes', MultinomialNB())
 ])
 
-#Prepare train labels:
-n_pos_samples_train = len(pos_train_raw)
-n_neg_samples_train = len(neg_train_raw)
-y_train = np.concatenate((np.ones((n_pos_samples_train)), np.zeros((n_neg_samples_train))))
+#Prepare train labels (train data + test data):
+n_pos_samples = len(pos_train_raw + pos_test_raw)
+n_neg_samples = len(neg_train_raw + neg_test_raw)
+y_train = np.concatenate((np.ones((n_pos_samples)), np.zeros((n_neg_samples))))
 
-pipe.fit(pos_train_raw + neg_train_raw, y_train)
+pipe.fit(pos_train_raw + pos_test_raw + neg_train_raw + neg_test_raw, y_train)
 
 file_name = 'pipeline_naivebayes.joblib'
 path = Path('../models')
