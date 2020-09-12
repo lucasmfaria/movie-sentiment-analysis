7705e0ddebc4c8d95b79db46725ea01025fd5759
Lucas Faria <lucas.meneghesso.faria@gmail.com>
2020-09-12 18:56:24 -0300

    Added Flask app to serve the model to the user. Updated readme and requirements.txt

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Study of sentiment analysis techniques using Stanford "Large Movie Review Dataset".
 
 The dataset can be downloaded in: http://ai.stanford.edu/~amaas/data/sentiment/
+
+The project steps reproduced here are:
+> Study: jupyter notebooks for the data exploration, model construction, model selection and model evaluation
+>
+> Training/prototyping: script creation for training prodution model
+> 
+> Deploying: use of Flask to serve the model to the user
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from wtforms import Form, validators, StringField
+from pathlib import Path
+from joblib import load
+
+pred_model_path = Path('./models')
+pred_model_filename = 'pipeline_naivebayes.joblib'
+pred_model = load(pred_model_path / pred_model_filename)
+app = Flask(__name__)
+
+# Model
+class InputForm(Form):
+    r = StringField(validators=[validators.InputRequired()])
+
+# View
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        r = form.r.data
+        tokens = [r]
+        y = pred_model.predict(tokens)
+        if y == 1:
+            s = 'Positive'
+        elif y == 0:
+            s = 'Negative'
+    else:
+        s = None
+    return render_template("view.html", form=form, s=s)
+
+if __name__ == '__main__':
+    app.run(debug=True)
